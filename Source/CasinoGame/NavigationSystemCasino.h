4bcc314f156bf88a437f9ee63c63088511c59f51
Safirl <loic.leforestier@mmibordeaux.com>
2024-08-04 01:55:50 +0200

    Adding zones (not working)

---
+++ Source/CasinoGame/NavigationSystemCasino.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NavigationSystem.h"
+#include "NavigationSystemCasino.generated.h"
+
+class UWorld;
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class CASINOGAME_API UNavigationSystemCasino : public UNavigationSystemV1
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable)
+	static void BuildNav(UWorld* World);
+};
