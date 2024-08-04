0274cea485eed4fc958c6ef0cf94d0623e8966a6
Safirl <loic.leforestier@mmibordeaux.com>
2024-08-04 10:59:59 +0200

    Changing door so we don't collider on them

---
+++ Source/CasinoGame/NavigationRebuilder.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NavigationRebuilder.h"
+#include "NavigationSystem.h"
+
+
+// Sets default values
+ANavigationRebuilder::ANavigationRebuilder()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ANavigationRebuilder::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANavigationRebuilder::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ANavigationRebuilder::RebuildNavMesh() const
+{
+	if (UNavigationSystemV1* NavSys = FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld()))
+	{
+		NavSys->Build();
+	}
+}
+
